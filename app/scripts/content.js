7cc62094816c1730c6f5c83f02f0be827e58e382
Edoardo Bianchi <edotesi@gmail.com>
2015-01-25 15:49:35 +0100

    Minor changes, project description

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -3,64 +3,68 @@
 /*TODO
 
 	- remove handlebars, too heavy
-	- style tab list
 
-*/
+	*/
 
-var listInPage = false;
-var containerProps = {
-	tag: 'ul',
-	id: 'chrome-ext-history-tabs'
-};
+(function() {
 
-function tabsList(tabs) {
-	console.log('show tablist!');
+	var listInPage = false;
+	var containerProps = {
+		tag: 'ul',
+		id: 'chrome-ext-history-tabs'
+	};
 
-	var container = document.getElementById(containerProps.id);
+	function tabsList(tabs) {
+		console.log('show tablist!');
 
-	if (!container) {
-		container = document.createElement(containerProps.tag);
-		container.id = containerProps.id;
-		document.body.appendChild(container);
+		var container = document.getElementById(containerProps.id);
+
+		if (!container) {
+			container = document.createElement(containerProps.tag);
+			container.id = containerProps.id;
+			document.body.appendChild(container);
+		}
+
+		container.innerHTML = templateList(tabs);
+		listInPage = true;
 	}
 
-	container.innerHTML = templateList(tabs);
-	listInPage = true;
-}
-
-function removeTabsList() {
-	var container = document.getElementById(containerProps.id);
-	if (!!container) {
-		container.parentNode.removeChild(container);
+	function removeTabsList() {
+		var container = document.getElementById(containerProps.id);
+		if (!!container) {
+			container.parentNode.removeChild(container);
+		}
+		listInPage = false;
 	}
-	listInPage = false;
-}
 
-function templateList(tabs) {
-	var source   = '{{#each this}}<li class="tab {{#if _highlighted}}highlighted{{/if}}">{{title}}</li>{{/each}}';
-	var template = Handlebars.compile(source);
-	console.log(tabs);
-	var html = template(tabs);
+	function templateList(tabs) {
+		var source = '{{#each this}}<li class="tab {{#if _highlighted}}highlighted{{/if}}">{{title}}</li>{{/each}}';
+		var template = Handlebars.compile(source);
+		console.log(tabs);
+		var html = template(tabs);
 
-	return html;
-}
+		return html;
+	}
 
-(function listenForCtrlRelease() {
-    window.addEventListener('keyup', function(e) {
-        if (e.keyCode === 17 && listInPage) {
-            console.log('remove list');
-            removeTabsList();
-            chrome.runtime.sendMessage({type: 'activateTab'}, function(response) {
-            	console.log(response.farewell);
-            });
-        }
-    });
+	(function listenForCtrlRelease() {
+		window.addEventListener('keyup', function(e) {
+			if (e.keyCode === 17 && listInPage) {
+				console.log('remove list');
+				removeTabsList();
+				chrome.runtime.sendMessage({
+					type: 'activateTab'
+				}, function(response) {
+					console.log(response.farewell);
+				});
+			}
+		});
+	})();
+
+	/* Listen for messages */
+	chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+		/* If the received message has the expected format... */
+		if (msg.type && (msg.type === 'showTabList')) {
+			tabsList(msg.tabs);
+		}
+	});
 })();
-
-/* Listen for messages */
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    /* If the received message has the expected format... */
-    if (msg.type && (msg.type === 'showTabList')) {
-        tabsList(msg.tabs);
-    }
-});
